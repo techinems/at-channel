62bfe54304bfbe8bfdff6c8fd2ff132b8440a25b
Dan Bruce <dan@ddbruce.com>
2019-05-06 18:57:47 -0500

    logan-proofing and rework how initial message/ephemeral note is sent

--- handlers/slashCommands.js
+++ handlers/slashCommands.js
@@ -5,7 +5,7 @@ const md5 = require("md5");
 const {
   app: {
     client: {
-      chat: { postEphemeral }
+      chat: { postEphemeral, postMessage }
     }
   }
 } = require("../utilities/bolt.js");
@@ -17,6 +17,14 @@ const TOKEN = process.env.SLACK_BOT_TOKEN;
 const slashChannel = async ({
   command: { channel_name, channel_id, user_id, text }
 }) => {
+  if (text == "" || text == "&gt;" || text == "&gt;&gt;&gt;") {
+    postEphemeral({
+      token: TOKEN,
+      channel: channel_id,
+      user: user_id,
+      text: ":thinking_face: Maybe try actually writing something?"
+    });
+  }
   switch (channel_name) {
     case "general":
     case "alerts":
@@ -32,12 +40,20 @@ const slashChannel = async ({
         token: TOKEN,
         channel: channel_id,
         user: user_id,
+        text:
+          ":raised_hands: Thanks for your request! It's been sent to the moderators for approval."
+      });
+      postMessage({
+        token: TOKEN,
+        channel: user_id,
+        user: user_id,
+        text: "Your message has been sent to the moderators for approval.",
         blocks: [
           {
             type: "section",
             text: {
               type: "mrkdwn",
-              text: `:raised_hands: *Thanks for your message!* It has been sent to the moderators for approval.\nYou wrote:\n>>>${text}`
+              text: `You requsted an at-channel messasge to be sent to <#${channel_id}>. It has been sent to the moderators for approval.\nYou wrote:\n>>>${text}`
             }
           },
           {
@@ -60,7 +76,7 @@ const slashChannel = async ({
                   text: "Cancel @channel request"
                 },
                 style: "danger",
-                action_id: `DEL_${requestId}`
+                action_id: `CAN_${requestId}`
               }
             ]
           }
